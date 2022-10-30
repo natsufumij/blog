a415bb4cb55d1423c58a558eb35a03cc68c9aad1
natsufumij <natsufumij@yeah.net>
2022-10-30 10:10:05 +0800

    init

---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,8 @@
+import {defineUserConfig} from 'vuepress';
+
+export default defineUserConfig({
+    base: '/blog/',
+    lang: 'zh-CN',
+    title: '你好, Vue Press!',
+    description: '这是我的第一个VuePress站点'
+});
